ef91542f8a0a7c105e2ea36bff31d53b207abdce
sachinDev07 <sachinkrj077@gmail.com>
2024-08-25 11:37:08 +0530

    Setup done for database connection using mongodb

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./db/db");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: "http://localhost:3000",
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
-}));
+  }),
+);
 
-app.listen(8000, () => {
-    console.log("Server is started!");
-})
+connectDB()
+  .then(() => {
+    console.log("Database connection established successfully");
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is started at port: ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongoose connection error: ", error);
+  });
