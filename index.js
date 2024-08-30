522d2ba5ad85fcd7bc6bbefb5fd6e930610a90e6
sachinDev07 <sachinkrj077@gmail.com>
2024-08-30 13:04:23 +0530

    Changed the origin for cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   }),
